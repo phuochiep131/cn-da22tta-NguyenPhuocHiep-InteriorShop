9d70a486982f7aa6793435ade4accc939f2458fa
phuochiep131 <nphhiep1301@gmail.com>
2025-10-02 21:26:32 +0700

    First commit for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,22 @@ import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import ScrollToTop from "./components/ScrollToTop"
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Header />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+        <main className="flex-grow py-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
 
         <Footer />
       </div>
